ac990bb5a40ff6b18f9c372479505bdabbb8db03
Dave Warnock <dave@warnock.me.uk>
2022-12-16 00:49:07 +0000

    after cargo fmt

--- src/local_system.rs
+++ src/local_system.rs
@@ -1,15 +1,15 @@
-// Dave's Dashboard Browser based system and world dashboard 
+// Dave's Dashboard Browser based system and world dashboard
 // Copyright (C) 2022  Dave Warnock dwarnock@gmail.com
 
 // This file is part of Dave's Dashboard.
 
-//Dave's Dashboard is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as 
+//Dave's Dashboard is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
 // published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-// Dave's Dashboard is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+// Dave's Dashboard is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License along with Dave's Dashboard. 
+// You should have received a copy of the GNU General Public License along with Dave's Dashboard.
 // If not, see <https://www.gnu.org/licenses/>.
 
 use sysinfo::{NetworkExt, ProcessExt, System, SystemExt};
@@ -33,7 +33,12 @@ pub fn info() {
     // Network interfaces name, data received and data transmitted:
     println!("=> networks:");
     for (interface_name, data) in sys.networks() {
-        println!("{}: {}/{} B", interface_name, data.received(), data.transmitted());
+        println!(
+            "{}: {}/{} B",
+            interface_name,
+            data.received(),
+            data.transmitted()
+        );
     }
 
     // Components temperature:
